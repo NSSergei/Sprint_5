302640863b75ab35fe1d5acc17a9ca188d5c8353
Sergei <ssnefedov97@mail.ru>
2025-10-07 19:24:37 +0300

    Внесение изменений

--- src/Main/DeliveryApp.java
+++ src/Main/DeliveryApp.java
@@ -68,53 +68,55 @@ public class DeliveryApp {
                 0 - Завершить"""
         );
 
-            int choice = Integer.parseInt(scanner.nextLine());
-            switch (choice) {
-                case 1:
-                    parcelType = ParcelType.STANDARD;
-                    break;
-                case 2:
-                    parcelType = ParcelType.PERISHABLE;
-                    break;
-                case 3:
-                    parcelType = ParcelType.FRAGILE;
-                    break;
-                case 0:
-                    return;
-                default:
-                    System.out.println("Нет такого типа посылок");
+        int choice = Integer.parseInt(scanner.nextLine());
+        switch (choice) {
+            case 1:
+                parcelType = ParcelType.STANDARD;
+                break;
+            case 2:
+                parcelType = ParcelType.FRAGILE;
+                break;
+            case 3:
+                parcelType = ParcelType.PERISHABLE;
+                break;
+            case 0:
+                return;
+            default:
+                System.out.println("Нет такого типа посылок");
 
-            }
+        }
 
 
         System.out.println("Введите название посылки");
-            String description = scanner.nextLine();
+        String description = scanner.nextLine();
 
         System.out.println(("Введите вес посылки"));
-            int weight = Integer.parseInt(scanner.nextLine());
+        int weight = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Введите адрес доставки");
-            String deliveryAddress = scanner.nextLine();
+        String deliveryAddress = scanner.nextLine();
 
         System.out.println("""
             Введите дату доставки
             В данном месяце 31 день
             """);
-            int sendDay = Integer.parseInt(scanner.nextLine());
+        int sendDay = Integer.parseInt(scanner.nextLine());
 
-            while (0 > sendDay || sendDay > 31){
-                System.out.println("В данном месяце 31 день вы ввели: " + sendDay +
-                        " Введите корретный день");
+        while (0 > sendDay || sendDay > 31){
+            System.out.println("В данном месяце 31 день вы ввели: " + sendDay +
+                    " Введите корретный день");
 
-                sendDay = Integer.parseInt(scanner.nextLine());
-            }
+            sendDay = Integer.parseInt(scanner.nextLine());
+        }
 
         int timeToLive = 0;
         Parcel parcel;
-        if (parcelType == ParcelType.FRAGILE) {
+        if (parcelType == ParcelType.PERISHABLE) {
             System.out.println("Укажите срок хранения скоропотящейся посылки");
             timeToLive = Integer.parseInt(scanner.nextLine());
             parcel = new PerishableParcel(description, weight, deliveryAddress, sendDay, parcelType, timeToLive);
+        } else if (parcelType == ParcelType.FRAGILE){
+            parcel = new FragileParcel(description, weight, deliveryAddress, sendDay, parcelType);
         } else {
             parcel = new StandardParcel(description, weight, deliveryAddress, sendDay, parcelType);
         }
